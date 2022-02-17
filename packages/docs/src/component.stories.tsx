8ad15b3946b18f7d12df7ad77cb5058725b0cce9
BoBoooooo <17746714@qq.com>
2022-02-17 11:50:34 +0800

    chore: add storybook

---
+++ packages/docs/src/component.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Demo from "./demo";
+
+export default {
+    title: 'Component',
+};
+
+
+export const Basic = () => {
+    return <Demo />;
+};
--- packages/examples/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Demo from './demo';
-
-ReactDOM.render(<Demo />, document.getElementById('app'));
